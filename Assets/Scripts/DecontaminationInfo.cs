9f53bc62423b4caed8c9598ed3ce171e51f185c3
alex n <alexn@outlook.de>
2025-10-13 18:20:25 +0200

    two types of contamination on items added. Automatic random spawns

--- Assets/Scripts/DecontaminationInfo.cs
+++ Assets/Scripts/DecontaminationInfo.cs
@@ -4,5 +4,22 @@ public class DecontaminationInfo : MonoBehaviour
 {
     public ToolTypes[] acceptedTypes;
     public ToolTypes toolType;
-    public Color clean;
+    public ContaminationSpot[] contaminationSpots;
+}
+
+[System.Serializable]
+public struct ContaminationSpot
+{
+    public bool visible;
+    public Vector2 pos;
+    public Texture2D texture;
+    public float intensity;
+
+    public ContaminationSpot(Vector2 pos, Texture2D texture, float intensity, bool visible)
+    {
+        this.pos = pos;
+        this.texture = texture;
+        this.intensity = intensity;
+        this.visible = visible;
+    }
 }
